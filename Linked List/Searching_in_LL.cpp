a3aa88a81cb8bc070ac731dbfe3cc2b00fda208f
Ankit-Shrestha911 <Ankitshrestha911@gmail.com>
2020-12-21 13:44:24 +0530

    Searching in LL

---
+++ Linked List/Searching_in_LL.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+using namespace std;
+
+class node{
+     public:
+     int data;
+     node* next;
+     node(int d){
+         data = d;
+         next = NULL;
+     }
+};
+
+node* searching(node* head, int key){
+    if(head == NULL){
+        return NULL;
+    }
+
+    if(head->data == key){
+        return head;
+    }
+    node* ans = searching(head->next,key);
+    
+    return ans;
+
+}
+
+void insertAtHead(node* &head, int d){
+    node* n = new node(d);
+    n->next = head;
+    head = n;
+}
+
+void display(node* head){
+    node* temp = head;
+    while(temp!=NULL){
+        cout<<temp->data<<"-->";
+        temp = temp->next;
+    }
+    cout<<"NULL"<<endl;
+}
+ 
+int main() 
+{
+    node* head = NULL;
+    insertAtHead(head,1);
+    insertAtHead(head,2);
+    insertAtHead(head,3);
+    display(head);
+
+    node* store  = searching(head,4);
+    if(store){
+        cout<<"Found "<<store->data;
+    }
+    else{
+        cout<<"Not found";
+    }
+
+     
+    return 0;
+}
