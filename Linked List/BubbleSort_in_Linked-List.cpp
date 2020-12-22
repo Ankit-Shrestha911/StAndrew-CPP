1b405db59674546e48dc612e7388c0c3f9cb6e46
Ankit-Shrestha911 <Ankitshrestha911@gmail.com>
2020-12-22 14:26:00 +0530

    Bubble Sort

---
+++ Linked List/BubbleSort_in_Linked-List.cpp
@@ -0,0 +1,124 @@
+#include<iostream>
+using namespace std;
+ 
+class node
+{
+public:
+    int data;
+    node *next;
+
+    node(int d)
+    {
+        data = d;
+        next = NULL;
+    }
+};
+
+int length(node* head){ // Calculating the length //
+
+    int cnt = 0;
+    while(head){
+    cnt++;
+    head = head->next;
+    }
+    return cnt;
+}
+
+ void insertAtTail(node *&head, int d) // inserting the node from tail //
+{
+
+    node *n = new node(d);
+    if (head == NULL)
+    {
+        head = n;
+        return;
+    }
+
+    node *temp = head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = n;
+}
+
+void insertAtHead(node *&head, int d) // Inserting the node from head //
+{
+    if (head == NULL)
+    {
+        node *n = new node(d);
+        head = n;
+        return;
+    }
+    else
+    {
+        node *n = new node(d);
+        n->next = head;
+        head = n;
+    }
+}
+
+void display(node *head) // Traversing  linked list //
+{
+    node *temp = head;
+    while (temp)
+    {
+        cout << temp->data << "-->";
+        temp = temp->next;
+    }
+    cout << "NULL" << endl;
+}
+
+void bubbleSort(node* &head){
+    node*prev = NULL,*current = head, *n;
+    int len = length(head);
+
+    for(int i=0; i<len-1; i++){
+        current = head,prev = NULL;
+       
+        while(current!=NULL && current->next!=NULL){
+            n = current->next;
+            if(current->data > n->data){
+
+             if(prev == NULL){
+                // Head swapping //
+                current->next = n->next;
+                n->next  = current;
+                head = prev = n;
+             }
+             else{
+                 //Head is not swapping //
+                 prev->next = current->next;
+                 current->next = n->next;
+                 n->next = current;
+                 prev = n;
+             }
+            }
+            else{
+                prev = current;
+                current = current->next;
+
+            }
+
+
+        }
+    }
+
+
+}
+
+ int main()  
+{
+    node* head = NULL;
+    insertAtTail(head,1);
+    insertAtTail(head,5);
+    insertAtTail(head,2);
+    insertAtTail(head,3);
+    display(head);
+    cout<<endl;
+    bubbleSort(head);
+    display(head);
+
+     
+    return 0;
+}
